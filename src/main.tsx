c8a82d43941099840c08a3822930a52c43d1b3ec
Carlos <charlsef23@icloud.com>
2024-12-23 22:47:14 +0100

    styles v0

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './styles/style.css'
+import './styles/Navbar.css'
+import './styles/Hero.css'
+import './styles/About.css'
+import './styles/Projects.css'
 import { BrowserRouter } from 'react-router-dom'
 import App from './App'
 
