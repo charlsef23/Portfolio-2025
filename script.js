d2f9dece35fc96053210a1cd4cb0efb1a4516aa5
Carlos <charlsef23@icloud.com>
2025-01-01 21:02:24 +0100

    Resumen y portfolio

--- script.js
+++ script.js
@@ -65,7 +65,7 @@ const filterItems = document.querySelectorAll('[data-filter-item]');
 
 const filterFunc = function (selectedValue) {
     for(let i = 0; i < filterItems.length; i++) {
-        if(selectedValue == "all") {
+        if(selectedValue == "todos") {
             filterItems[i].classList.add('active');
         } else if (selectedValue == filterItems[i].dataset.category) {
             filterItems[i].classList.add('active');
